a14a6d802267e027923cb322f8bc8a895d45b0c9
elle <missanyayoung@outlook.com>
2019-08-19 13:43:15 -0700

    deployment

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express')
 var app = express()
 
 app.use (express.static('./public'));
+app.get('/', (request, response) => {
+    response.status(200).redirect('index.html');
+});
 
 const PORT = 3000
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
